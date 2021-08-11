785b1c7deda149e8b3ee481452d9b1545c31eb2f
T1b4lt <guillermosegfer@gmail.com>
2021-08-11 23:39:54 +0200

    Initial commit app.py

---
+++ app.py
@@ -0,0 +1,79 @@
+import json
+import os
+import logging
+import sys
+import config
+import requests
+from flask import Flask, request, Response, jsonify
+from flask_restful import Api, Resource, reqparse
+from flasgger import Swagger, swag_from
+
+
+# Setup Flask Server
+app = Flask(__name__)
+
+
+# Create an APISpec
+template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Fatsecret API",
+        "description": "A Restful API for Fatsecret food info",
+        "version": "0.1.1",
+        "contact": {
+            "name": "T1b4lt",
+            "github": "https://github.com/T1b4lt",
+        }
+    }
+}
+
+app.config['SWAGGER'] = {
+    'title': 'Fatsecret API',
+    'uiversion': 3,
+    "specs_route": "/swagger/"
+}
+swagger = Swagger(app, template=template)
+app.config.from_object(config.Config)
+api = Api(app)
+
+
+class Food(Resource):
+    def get(self, food_name):
+        """
+        get endpoint
+        ---      
+        tags:
+          - Flask Restful API
+        parameters:
+          - name: food_name
+            in: path
+            type: string
+            required: true
+            description: name of the food
+        responses:
+          500:
+            description: NOT OK
+          200:
+            description: OK
+            schema:
+              id: food
+              properties:
+                type:
+                  type: string
+                  description: Type of object
+                food_name:
+                  type: string
+                  description: The food we are looking for           
+        """
+        return jsonify({
+            "type": 'food',
+            "food_name": food_name
+        })
+
+
+# Api resource routing
+api.add_resource(Food, '/food/<string:food_name>')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
