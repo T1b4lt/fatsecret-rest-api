ad6fe2cc6386d34030adb3f741e35f334ace93cb
T1b4lt <guillermosegfer@gmail.com>
2021-08-12 22:29:14 +0200

    Created food model

--- resources/food.py
+++ resources/food.py
@@ -1,6 +1,12 @@
 class Food:
     def __init__(self, food_name):
         self.food_name = food_name
+        self.prot = 0.0
+        self.carbh = 0.0
+        self.fat = 0.0
+        self.kcal = 0.0
+        self.unit = ''
+        self.quantity = 0.0
 
     def to_json(self):
         return dict(title=self.food_name)
