48684791aab974affce6f4d7a73c49737f195fb4
T1b4lt <guillermosegfer@gmail.com>
2021-08-15 15:23:44 +0200

    Updated food model

--- resources/food.py
+++ resources/food.py
@@ -1,6 +1,7 @@
 class Food:
     def __init__(self, food_name):
         self.food_name = food_name
+        self.food_brand = ''
         self.protein = 0.0
         self.carbs = 0.0
         self.fat = 0.0
